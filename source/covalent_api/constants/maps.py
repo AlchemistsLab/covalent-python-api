7b7d60e73efae87bff14a8516e09e010f80b8ed4
Lluis Casals Marsol <ktmlleska@users.noreply.github.com>
2021-08-02 15:53:06 +0200

    Revert "added uniswap clones endpoint (#1)"
    This reverts commit 1981fdd229ab31e22c476361ae16c91116b7ff21.

--- source/covalent_api/constants/maps.py
+++ source/covalent_api/constants/maps.py
@@ -23,12 +23,3 @@ AVAILABLE_CHAIN_IDS_CONTRACT_METADATA = {
     'Fuji C-Chain Testnet': '43113',
     'Fantom Opera Mainnet': '250'
 }
-
-#Available Dexes
-AVAILABLE_DEXES = {
-'sushiswap','sushiswap',
-'quickswap','quickswap',
-'pangolin','pangolin',
-'spiritswap','spiritswap',
-'spookyswap','spookyswap',
-}
